7fc790e48856c6c5da5da552252629f98895e577
Lukas_leandro <leandrolukas916@gmail.com>
2023-11-13 14:14:36 -0300

    implementing functions in controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
-    //
+    public function index() 
+    {
+        $contacts = Contact::all();
+        return view('contacts.index', compact('contacts'));
+    }
+
+    public function create()
+    {
+        return view('contacts.create');
+    }
+
+    public function edit() 
+    {
+        return view('contacts.edit');
+    }
+
+    public function delete()
+    {
+
+    }
 }
